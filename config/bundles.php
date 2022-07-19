d55ce60fcd070207cf03426c390a29f7e693d5ea
DmitryKirillov <esteps.kirillov@gmail.com>
2022-07-19 19:18:49 +0300

    Install jms/serializer-bundle and friendsofsymfony/rest-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
 ];
