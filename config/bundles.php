d9f2f5bba10fc93d6eb95f07a2c94495c802b166
DmitryKirillov <esteps.kirillov@gmail.com>
2022-07-20 13:58:55 +0300

    Install lexik/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
